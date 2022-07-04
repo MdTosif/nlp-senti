16cc8942e8fd6a6468d22ec7ad2f435668f49d61
MdTosif <mdtosif0mt@gmail.com>
2022-07-04 19:31:33 +0530

    cors

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ import json
 import os
 from flask import Flask, request, jsonify
 from nlpSentiment import *
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/", methods=["POST"])
 def isNegative():
+    # print('x\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\n')
     record = json.loads(request.data)
-    a = isNegativeNLP(record['text'])
-    return jsonify({"isNegative": a})
+    tmp = dict(record)
+    # print(record)
+    for k,v in tmp.items():
+        record[k]['isNegative'] = isNegativeNLP(v['text'])
+    return jsonify(record)
 
 
 # app.run( po)
